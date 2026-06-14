39c5a6fa6bdd9cad4ad903d6d4bc98d29f7eceae
akbulet448-sketch <akbulet448@gmail.com>
2026-06-14 10:11:39 +0530

    Update page.tsx

--- teleplus-vb_f2cfSTXabJb/app/page.tsx
+++ teleplus-vb_f2cfSTXabJb/app/page.tsx
@@ -1,5 +1,21 @@
-import { TelePulsApp } from "@/components/telepuls/telepuls-app"
+'use client';
+import { useEffect } from 'react';
+import { TelePulsApp } from "@/components/telepuls/telepuls-app";
 
 export default function HomePage() {
-  return <TelePulsApp />
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      try {
+        // @ts-ignore
+        if (window.Pi) {
+          // @ts-ignore
+          window.Pi.init({ version: "2.0" });
+        }
+      } catch (error) {
+        console.error("Pi SDK init failed", error);
+      }
+    }
+  }, []);
+
+  return <TelePulsApp />;
 }
